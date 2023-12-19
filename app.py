ce8cfe2b03365f21c00557d11f793aa5e63b6043
tuan <tuan.nguyen@datics-consulting.com>
2023-12-19 17:33:01 +0100

    storing uploaded file and meta data

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # app.py
 
+import base64
 import io
 
 import dash
@@ -263,6 +264,75 @@ def identify_bad_data(df):
     # Create a new column 'is_bad' to mark bad data rows
     df['is_bad'] = condition1 | condition2
     return df
+
+
+
+import base64
+import os
+import boto3
+from dash.exceptions import PreventUpdate
+
+# Initialize the S3 client
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.environ.get('AWS_DEFAULT_REGION')
+)
+
+import boto3
+from dash.exceptions import PreventUpdate
+import datetime
+
+# Initialize DynamoDB client
+dynamodb = boto3.resource(
+    'dynamodb',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+    region_name=os.environ.get('AWS_DEFAULT_REGION')
+)
+
+# Reference to your DynamoDB table
+metadata_table = dynamodb.Table('WoodudFileUploadMetadata')
+
+@dash_app.callback(
+    Output('output-container', 'children'),
+    [Input('upload-data', 'contents')],
+    [State('upload-data', 'filename')]
+)
+def upload_file_to_s3(list_of_contents, list_of_names):
+    if list_of_contents is None:
+        raise PreventUpdate
+
+    messages = []  # To store messages for each file upload
+
+    for contents, filename in zip(list_of_contents, list_of_names):
+        # Decode the file content from base64
+        content_type, content_string = contents.split(',')
+        decoded = base64.b64decode(content_string)
+
+        try:
+            # Upload the file to the S3 bucket
+            s3_client.put_object(
+                Bucket='woodud',
+                Key=filename,
+                Body=decoded
+            )
+
+            # Store metadata in DynamoDB
+            metadata_table.put_item(
+                Item={
+                    'filename': filename,
+                    'uploadTime': str(datetime.datetime.now()),
+                    'user': 'user-identifier'  # You can modify this based on your app's user system
+                }
+            )
+
+            messages.append(html.Div(f'Successfully uploaded {filename} to S3 and recorded metadata.'))
+        except Exception as e:
+            messages.append(html.Div(f'Failed to upload {filename}. Error: {e}'))
+
+    return html.Div(messages)
 
 
 # Run the app
